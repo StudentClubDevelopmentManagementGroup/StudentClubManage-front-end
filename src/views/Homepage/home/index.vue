2a9128f43da7326a3ff28b08506978af0fb1b8f4
A1033A <923712175@qq.com>
2024-06-20 21:03:54 +0800

    首页点击名字出现undefined问题

--- src/views/Homepage/home/index.vue
+++ src/views/Homepage/home/index.vue
@@ -5,6 +5,7 @@ import baseApi from "@/api/base.js";
 import announcementApi from "@/api/announcement.js";
 
 import { IconifyIconOnline } from "@/components/Icon";
+import { list } from "postcss";
 
 const list1 = ref([]);
 const list2 = ref([]);
@@ -22,6 +23,7 @@ const fetchDataList1 = () => {
       .getBaseInfo(body)
       .then((data) => {
         list1.value = data.records;
+        console.log(list1.value);
       })
       .catch((err) => {
         console.warn(err);
@@ -94,7 +96,8 @@ onMounted(() => {
           </template>
 
           <template v-if="list1.length !== 0" v-for="item in list1" :key="item">
-            <router-link :to="`/homepage/list?club_name=${item.club_name}`">
+            {{ console.log("item", item) }}
+            <router-link :to="`/homepage/list?club_name=${item.name}`">
               <div
                 class="cursor-pointer flex group rounded-lg p-2 text-black hover:bg-sky-500 hover:ring-sky-500"
               >
